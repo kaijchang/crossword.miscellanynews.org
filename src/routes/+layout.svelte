8393c48db30b7cafa5a97acb1efb90dc7d48f2e3
Kai Chang <kaijchang@gmail.com>
2024-10-21 14:41:11 -0400

    container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,45 +7,53 @@
     inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-<div class="header">
-    <a href="/">
-        <img class="logo" src="/logo.svg" alt="Miscellany News Logo" />
-    </a>
-    <a href="/">
-        <h1 class="title">Crossword</h1>
-    </a>
-    <!-- <div class="nav-links">
-        <h4>
-            &middot;
-        </h4>
-        <h4>
-            <a href="/leaderboard">
-                Leaderboard
-            </a>
-        </h4>
-        <h4>
-            &middot;
-        </h4>
-        <h4>
-            <a href="/sign-in">
-                Sign In
-            </a>
-        </h4>
-        <h4>
-            &middot;
-        </h4>
-    </div> -->
-</div>
-
-<slot></slot>
-
-<div class="footer">
-    <p>
-        Questions? Issues? Email <b><a href="mailto:kchang@vassar.edu">kchang@vassar.edu</a></b>
-    </p>
+<div class="container">
+    <div class="header">
+        <a href="/">
+            <img class="logo" src="/logo.svg" alt="Miscellany News Logo" />
+        </a>
+        <a href="/">
+            <h1 class="title">Crossword</h1>
+        </a>
+        <!-- <div class="nav-links">
+            <h4>
+                &middot;
+            </h4>
+            <h4>
+                <a href="/leaderboard">
+                    Leaderboard
+                </a>
+            </h4>
+            <h4>
+                &middot;
+            </h4>
+            <h4>
+                <a href="/sign-in">
+                    Sign In
+                </a>
+            </h4>
+            <h4>
+                &middot;
+            </h4>
+        </div> -->
+    </div>
+    
+    <slot></slot>
+    
+    <div class="footer">
+        <p>
+            Questions? Issues? Email <b><a href="mailto:kchang@vassar.edu">kchang@vassar.edu</a></b>
+        </p>
+    </div>
 </div>
 
 <style>
+    .container {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+
     .header, .footer {
         background: var(--color-primary);
         display: flex;
@@ -57,6 +65,10 @@
 
     .logo {
         max-width: 100%;
+    }
+
+    .footer {
+        margin-top: auto;
     }
 
     .nav-links {
